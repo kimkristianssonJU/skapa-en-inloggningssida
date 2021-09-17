18bad4f09fe70bb37dae2a73a6d3ee19cce187e1
Kim Kristiansson <krki21cn@student.ju.se>
2021-09-17 23:41:22 +0200

    Pushar inloggningsmöjlighet

--- README.md
+++ README.md
@@ -17,7 +17,13 @@ Min reflektion:
 
 I min JS-script har jag fokuserat på att arbeta med objekt. Jag skickade tidigare in en lösning på uppgiften, den lösningen var mer funktionsorienterad än denna. Jag personligen föredrar en lösning som är objektorienterad, likt denna. Koden blir mer dynamisk och hanterbar på ett mycket trevligare sätt, särskilt i ett större project. Koden känns lite mer komplex, men samtidigt så skapar den ett bra utgångsläge att utveckla vidare med.
 Jag har också haft i åtanke att funktionerna ska vara återanvändbar. Objektet 'alertMsg' går till exempel att pusha vilket meddelande som helst, 'serializeObjekt()' och 'deserializeObjekt()' är inte bundet till just ett objekt från klassen 'User'. Det går alltså att omvandla andra objekt till localStorage.
-Utgångsläget att vidareutveckla är som sagt lovande; härifrån går det med lätthet att möjliggöra för användaren att skapa nya konton eller byta användarnamn/lösenord för aktiv användare.
+
+Jag har gjort en demostration av vad upplägget är kapabelt till. Användaren har möjlighet att skapa ett nytt konto som lagras i local storage.
+
+Vissa kriterier finns:\
+    * Användaren ska ha mer än 4 karaktärer i namn och lösen\
+    * Användaren ska använda ett namn som inte redan är taget\
+    * Användaren får inte använda mellanslag i verken lösen eller namn
 
 Jag har valt att inte göra någon styling då det var valfritt. Kalla mig lat, men skönheten ligger på insidan även här. Dessutom kommer det fler möjligheter att skryta om CSS-kunskap senare.
 
--- script.js
+++ script.js
@@ -18,20 +18,16 @@ class User {
 
 // Objekt: Hanterar användaruppgifter
 const user1 = new User('test', '1234');
-const user2 = new User('test2', '5678');
 
-let userArray = [user1, user2];
+let userArray = [user1];
 
 // Objekt: Hanterar meddelanden menat för användaren
 let alertMsg = {
-    isActive: false,
     paraElement: document.createElement('p'),
+
     push: function(messageStr) {
-        if(!this.isActive) {
-            this.paraElement.textContent = messageStr;
-            contentContainer.appendChild(this.paraElement);
-            this.isActive = true;
-        }
+        this.paraElement.textContent = messageStr;
+        contentContainer.appendChild(this.paraElement);
     },
     clear: function() {
         this.paraElement.remove();
@@ -147,4 +143,146 @@ function getStoredObject(key) {
     }
 
     return deserializedObj;
+}
+
+////////////////
+// Nytt konto //
+////////////////
+
+/* 
+Här nedanför följer kod som egentligen är överflödig för uppgifter.
+Jag blev sugen att använda mig av objekten på det sätt som jag i 
+åtanke hade när jag började skriva.
+Dessvärre kom jag på dessa idéer alldeles för sent för att kunna
+göra mer utförligt, med fler funktioner. Men tid fanns inte.
+Jag inser att det fungerade som jag hade förväntat mig.
+Koden är i detta fallet är experimentell. 
+Den är inte vacker, men den fungerar :)
+
+I stora drag: Här tillåts användaren att göra ett nytt konto.
+Det nya kontot lagras i localStorage och användare kan
+förvänta sig kunna komma tillbaka och logga in igen.
+ 
+Vissa kriterier finns:
+    * Användaren ska ha mer än 4 karaktärer i namn och lösen
+    * Användaren ska använda ett namn som inte redan är taget
+    * Användaren får inte använda mellanslag i verken lösen eller namn
+    
+Nu är klockan 23.35, fredag den 17:e och jag behöver pusha 
+*/
+
+let signInBtn = document.getElementById("sign-in");
+let userArrayKey = "arrayOfUsers";
+
+if(getStoredObject(userArrayKey)) {
+    let addedUserArray = getStoredObject(userArrayKey);
+
+    for(let i = 0; i < addedUserArray.length; i++) {
+        if(addedUserArray[i].username !== "test")
+        {
+            userArray.push(addedUserArray[i]);
+        }
+    }
+}
+
+signInBtn.addEventListener("click", function() {
+    loginScreen.setActive(false);
+    createAccountScreen.setActive(true);
+});
+
+let createAccountScreen = {
+    setActive: function(activate) {
+        if(activate) {
+            const createAccountContainer = document.createElement("div");
+            const createAccountTitle = document.createElement("h1");
+            const confirmBtn = document.createElement("button");
+            const cancelBtn = document.createElement("button");
+            const chooseUsernameInput = document.createElement("input");
+            const choosePasswordInput = document.createElement("input");
+            const confirmPasswordInput = document.createElement("input");
+            
+            confirmBtn.textContent = "Fortsätt";
+            cancelBtn.textContent = "Avbryt";
+            createAccountTitle.textContent = "Skapa Konto";
+
+            chooseUsernameInput.placeholder = "Användarnamn";
+            choosePasswordInput.placeholder = "Lösenord";
+            confirmPasswordInput.placeholder = "Bekräfta Lösenord";
+
+            choosePasswordInput.type = "password";
+            confirmPasswordInput.type = "password";
+
+            createAccountContainer.classList.add("create-account-container");
+            
+
+            contentContainer.appendChild(createAccountContainer);
+            createAccountContainer.appendChild(createAccountTitle);
+            createAccountContainer.appendChild(chooseUsernameInput);
+            createAccountContainer.appendChild(choosePasswordInput);
+            createAccountContainer.appendChild(confirmPasswordInput);
+            createAccountContainer.appendChild(confirmBtn);
+            createAccountContainer.appendChild(cancelBtn);
+
+            chooseUsernameInput.style.display = "block";
+            choosePasswordInput.style.display = "block";
+            confirmPasswordInput.style.display = "block";
+
+            console.log(userArray);
+        
+            confirmBtn.addEventListener("click", function(){
+                if(isAcceptableInformation(chooseUsernameInput.value, choosePasswordInput.value, confirmPasswordInput.value)) {
+                    newUser = new User(chooseUsernameInput.value, chooseUsernameInput.value);
+                    console.log(newUser);
+                    userArray.push(newUser);
+                    storeObject(userArrayKey, userArray);
+                    
+                    chooseUsernameInput.value = "";
+                    choosePasswordInput.value = "";
+                    confirmPasswordInput.value = "";
+
+                    createAccountScreen.setActive(false);
+                    welcomeScreen.setActive(true, newUser);
+                }
+                else {
+                    choosePasswordInput.value = "";
+                    confirmPasswordInput.value = "";
+
+                }
+            });
+
+            cancelBtn.addEventListener("click", function(){
+                loginScreen.setActive(true);
+                createAccountScreen.setActive(false);
+            });
+        }
+        else {
+            contentContainer.removeChild(contentContainer.querySelector(".create-account-container"));
+            alertMsg.clear();
+        }
+    }
+}
+
+function isAcceptableInformation(usernameInput, passwordInput, confirmInput)
+{
+    isAccepted = true;
+    if(usernameInput.length < 4 || passwordInput.length < 4) {
+        alertMsg.push("Användarnamn och lösenord måste innehålla minst 4 karaktärer.");
+        isAccepted = false;
+    }
+    if(usernameInput.indexOf(' ') > 0 || passwordInput.indexOf(' ') > 0) {
+        alertMsg.push("Användarnamn och lösenord får inte inehålla mellanslag.");
+        isAccepted = false;
+    }
+    if(confirmInput !== passwordInput) {
+        alertMsg.push("Lösenorden måste matcha.");
+        isAccepted = false;
+    }
+    for(let i = 0; i < userArray.length; i++) {
+        if(userArray[i].username === usernameInput) {
+            alertMsg.push("Detta användarnamn används redan");
+            isAccepted = false;
+        }
+    }
+
+    return isAccepted;
 }
